36cdf9f047dba3076deac1f802d0c971bbf082c9
Максим <akaikinmax@mail.ru>
2024-01-09 16:24:51 +0700

    Реализована верстка шапки

--- sneakershop/src/App.vue
+++ sneakershop/src/App.vue
@@ -1,11 +1,21 @@
 <script setup>
+import Header from "./components/Header.vue";
 
 </script>
 
 <template>
-    <div></div>
+    <div class="wrapper">
+        <Header/>
+    </div>
 </template>
 
-<style scoped>
+<style scoped lang="sass">
+.wrapper
+    width: 80%
+    height: 100vh
+    margin: 0 auto
+    margin-top: 20px
+    border-radius: 35px
+    background-color: #fff
 
 </style>
---
+++ sneakershop/src/components/Header.vue
@@ -0,0 +1,65 @@
+
+<template>
+    <header class="header">
+        <div class="header__logo">
+            <img src="../../public/logo.png" alt="logo">
+            <div>
+                <h2 class="header__title">vue sneakers</h2>
+                <p class="header__subtitle">Магазин лучших кроссовок</p>
+            </div>
+        </div>
+        <ul class="navigation">
+            <li class="navigation__item">
+                <img src="../../public/cart.svg" alt="cart">
+                <b>1205 руб.</b>
+            </li>
+            <li class="navigation__item">
+                <img src="../../public/heart.svg" alt="heart">
+                <span>Закладки</span>
+            </li>
+            <li class="navigation__item">
+                <img src="../../public/profile.svg" alt="profile">
+                <span>Профиль</span>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<style scoped lang="sass">
+.header
+    display: flex
+    justify-content: space-between
+    padding: 20px 35px
+    border-bottom: 1px solid #EAEAEA
+    &__logo
+        display: flex
+        align-items: center
+        gap: 1rem
+        img
+            width: 40px
+    &__title
+        color: #000
+        font-size: 20px        
+        font-weight: 700
+        line-height: normal
+        text-transform: uppercase
+    &__subtitle
+        color: #9D9D9D
+        font-size: 14px
+        font-weight: 400
+        line-height: normal
+
+.navigation
+    display: flex
+    gap: 2.5rem
+    align-items: center
+    &__item
+        display: flex
+        align-items: center
+        gap: 0.75rem
+        cursor: pointer
+        color: rgb(107, 114, 128)
+        &:hover
+            color: rgb(0, 0, 0)
+            font-weight: bold
+</style>
